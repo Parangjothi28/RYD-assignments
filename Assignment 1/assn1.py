628f9392074bc2492f2b2281dd3e86046b7ffc29
Parangjothi C <37563679+Parangjothi28@users.noreply.github.com>
2020-07-07 10:05:59 +0530

    Add files via upload
    Extra Boxes are present. However further dilation of image would merge multiple questions into 1 box.

---
+++ Assignment 1/assn1.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+# Load image and remove watermark
+img = cv2.imread('inp.png')
+alpha = 2.5
+beta = -160
+new = alpha * img + beta
+new = np.clip(new, 0, 255).astype(np.uint8)
+
+# Grayscale, Gaussian blur, Otsu's threshold of Image
+gs = cv2.cvtColor(new, cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gs, (7,7), 0)
+thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+# Create rectangular structuring element and dilate
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+dilate = cv2.dilate(thresh, kernel, iterations=6)
+
+# Find contours and draw rectangle
+contours = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours = contours[0] if len(contours) == 2 else contours[1]
+for c in contours:
+    x,y,w,h = cv2.boundingRect(c)
+    cv2.rectangle(img, (x, y), (x + w, y + h), (36,255,12), 2)
+
+cv2.imshow('thresh', thresh)
+cv2.imshow('dilate', dilate)
+cv2.imshow('image', img)
+cv2.imwrite('output.png',img)
+cv2.waitKey()
