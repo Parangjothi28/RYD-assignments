2370e1681ddd194e93699adc6e96fe4110c04f8c
Parangjothi C <37563679+Parangjothi28@users.noreply.github.com>
2020-07-07 13:15:36 +0530

    Update assn1.py

--- Assignment 1/assn1.py
+++ Assignment 1/assn1.py
@@ -8,10 +8,15 @@ beta = -160
 new = alpha * img + beta
 new = np.clip(new, 0, 255).astype(np.uint8)
 
-# Grayscale, Gaussian blur, Otsu's threshold of Image
+# Grayscale, Gaussian blur and Filter2D, Otsu's threshold of Image
 gs = cv2.cvtColor(new, cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gs, (7,7), 0)
-thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+# ALternate blurring to blur more
+kernel2 = np.ones((5,5),np.float32)/25
+dst = cv2.filter2D(gs,-1,kernel2)
+
+thresh = cv2.threshold(dst, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
 # Create rectangular structuring element and dilate
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
@@ -22,7 +27,9 @@ contours = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 contours = contours[0] if len(contours) == 2 else contours[1]
 for c in contours:
     x,y,w,h = cv2.boundingRect(c)
-    cv2.rectangle(img, (x, y), (x + w, y + h), (36,255,12), 2)
+    # Ignoring smaller rectangles
+    if w*h > 2000:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (36,255,12), 2)
 
 cv2.imshow('thresh', thresh)
 cv2.imshow('dilate', dilate)
