029d010b5014237bda8dc079c17c8d9a3d3ab1e0
Parangjothi C <37563679+Parangjothi28@users.noreply.github.com>
2020-07-09 11:47:15 +0530

    Update mod_assn1.py

--- Assignment 1/mod_assn1.py
+++ Assignment 1/mod_assn1.py
@@ -19,13 +19,13 @@ n1 = 0
 n2 = 0
 m1 = 0
 m2 = 0
-print(len(lines))
+#print(len(lines))
 for i in range(len(lines)-1):
     x1, y1, x2, y2 = lines[i][0]
     #print(x1,y1)
     #Checking for horizontal line. Here, we focus on vertical lines.
     if(x1!=x2):
-        print(x2-x1)
+        #print(x2-x1)
         if y1 == y2:
             if y1 > maxy and x2-x1 > 200:
                 maxy = y1
@@ -46,32 +46,58 @@ y2 = img.shape[0]
 cv2.rectangle(new,(x1,y1),(x2,y2),(255,255,255),-1)
 
 
-gs = cv2.cvtColor(new, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gs, (7,7), 0)
+gs = cv2.cvtColor(new[miny:maxy,:], cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gs, (31,131), 0)
 
 # ALternate blurring to blur more
 kernel2 = np.ones((5,5),np.float32)/25
 dst = cv2.filter2D(gs,-1,kernel2)
 
-thresh = cv2.threshold(dst, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+thresh = cv2.threshold(blur, 0, 245, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 row,cols = thresh.shape
 print(row,cols)
 
 # Create rectangular structuring element and dilate
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-dilate = cv2.dilate(thresh, kernel, iterations=6)
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+dilate = cv2.dilate(thresh, kernel, iterations=3)
 
 # Find contours and draw rectangle
 contours = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 contours = contours[0] if len(contours) == 2 else contours[1]
+tot_cols = 0
+print(maxy-miny)
 for c in contours:
     x,y,w,h = cv2.boundingRect(c)
-    # Ignoring smaller rectangles
-    if w*h > 2000 and h > 40:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (36,255,12), 2)
+    # if w*h > 2000 and h > 40:
+    #     cv2.rectangle(img, (x, y), (x + w, y + h), (36,255,12), 2)
+    #print(h)
+    if h == maxy-miny:
+        tot_cols += 1
 
-cv2.imshow('thresh', thresh)
-cv2.imshow('dilate', dilate)
-cv2.imshow('image', img)
-cv2.imwrite('modoutput.png',img)
+print(tot_cols)
+
+part_wid = cols//tot_cols
+
+for i in range(tot_cols):
+    start = i*part_wid
+    fin = (i+1)*part_wid
+    blurry = cv2.GaussianBlur(gs[:,start:fin],(21,21),0)
+    thresh = cv2.threshold(blurry, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+    dilate = cv2.dilate(thresh, kernel, iterations=5)
+
+    contours = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+    for c in contours:
+        x,y,w,h = cv2.boundingRect(c)
+        if w*h > 2000 and h > 40:
+            cv2.rectangle(imgc,(start,y+miny),(fin,y+miny+h),(0,255,0),2)
+    cv2.imshow('thresh', thresh)
+    cv2.imshow('dilate', dilate)
+cv2.imwrite('modoutput.png',imgc)
+cv2.imshow('img2',imgc)
 cv2.waitKey()
+
+cv2.waitKey()
+
